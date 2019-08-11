a3be20c215b970bd39f60265d12e8a9a477530ef
anshchurov <anshchurov@gmail.com>
2019-08-11 14:41:53 +0300

    [add] getting real VIN hint

---
+++ src/main/java/ru/aplana/XAKATOH/VINRealGetting.java
@@ -0,0 +1,9 @@
+package ru.aplana.XAKATOH;
+
+public class VINRealGetting {
+
+    private static String getVINFromREST(){
+        //TODO: send rest by http://randomvin.com/getvin.php?type=real
+        return "";
+    }
+}
